7da0711839e421099161633d3d51f166b14e285d
Sufyan Mansuri <sufyan8834@gmail.com>
2022-07-19 13:23:50 +0530

    Change favicon based on player input

--- script.js
+++ script.js
@@ -24,6 +24,15 @@ function play(e) {
   computerSelection = computerPlay();
   computerSelectionEmoji = getEmoji(computerSelection);
   playerSelectionEmoji = getEmoji(playerSelection);
+
+  // Change favicon based on player input
+  const favicon = document.querySelector("link[rel=icon]");
+  favicon.setAttribute(
+    "href",
+    `data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>${playerSelectionEmoji}</text></svg>`
+  );
+
+  // Calculate and display game result
   const selectionDiv = document.querySelector(".selection");
   selectionDiv.textContent = `You: ${playerSelectionEmoji} Computer: ${computerSelectionEmoji}`;
   winner = playRound(playerSelection, computerSelection);
